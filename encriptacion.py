6ad94fffccb73a1dbe18f420c702c8f670f506dc
MartinPaGarcia <a01066569@itesm.mx>
2023-09-11 00:28:42 -0600

    Se importó el archivo Vigenere para acceder global

--- encriptacion.py
+++ encriptacion.py
@@ -1,5 +1,11 @@
+import vigenere
+
 # Elaborado por Juan Pablo Ruiz y Martin Palomares
 # Problema 2 
+# RECUERDA CORRER PRIMERO EL ARCHIVO VIGENERE.PY PARA PODER ACCEDER A LA VARIABLE GLOBAL laLlaveAmo
+# Sabemos que es el orden correcto, pero los hackers no queremos que adivinene nuestro siguiente...
+
+
 def elCifrador(texto, clave):
     textoCifrado = ""
     alfabeto = "abcdefghijklmnopqrstuvwxyz " # 27 caracteres contando el espacio
@@ -30,7 +36,9 @@ def podremosDescifrar(textoCifrado, clave):
         
     return textoDescifrado
 
+
 def main():
+    vigenere.main() #Traemos la funcion main del archivo vigenere.py
     texto_prueba = "Un joven padawan aun eres"
     clave_prueba = "yoda"
     textoCifrado = elCifrador(texto_prueba, clave_prueba)
@@ -40,7 +48,23 @@ def main():
     print(f"Texto cifrado: {textoCifrado}")
     print("")
     textoDescifrado = podremosDescifrar(textoCifrado, clave_prueba)
+    print("Prueba de descifrado: ")
     print(f"Texto descifrado: {textoDescifrado}")
+    print("")
+    print("")
+    # Recuerda que para que funcione se debe ejecutar primero el archivo vigenere.py
+    # De otra manera no se podra acceder a la variable global laLlaveAmo
+    print ("Ahora a descifrar el archivo cipher2.txt") 
+    clave_prueba = vigenere.laLlaveAmo 
+    with open("cipher2.txt", "r") as archivo:
+        contenido = archivo.read()
+    
+    texto_prueba = contenido
+
+    textoDescifrado = podremosDescifrar(contenido, clave_prueba)
+    print(f"Texto descifrado: {textoDescifrado}")
+
+    print (f"La clave es: {clave_prueba}")
 
 if __name__ == "__main__":
     main()
